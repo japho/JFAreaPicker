7098061fdcc00f279acf1fd35ed35ab407e10c40
Japho <japhoo@163.com>
2016-07-08 10:52:01 +0800

    Initial Commit

---
+++ JFAreaPicker/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JFAreaPicker
+//
+//  Created by 汪继峰 on 16/7/8.
+//  Copyright © 2016年 汪继峰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
